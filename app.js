e58052e0d7530096b4173c245e75c92c3fde0e29
Anthony <anthonyw22@gmail.com>
2018-04-16 21:16:07 +0100

    created first artist entry

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const bodyParser = require('body-parser');
+const artist = require('./controllers/Artist.js');
+
+console.log(artist);
 
 require('dotenv').config({
   path: path.join(__dirname, './settings.env'),
@@ -11,5 +14,6 @@ const app = express();
 mongoose.connect(process.env.DATABASE_CONN);
 app.use(bodyParser.json())
 app.get('/', (req, res) => res.send('Hello MongoDb!'));
+app.post('/Artist', artist.post);
 
 app.listen(3000, () => console.log('It works!'));
