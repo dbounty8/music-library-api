c2cd96e09fdce35ce4eaa85dc1ac1d7fc484ff5c
Anthony <anthonyw22@gmail.com>
2018-04-16 21:25:30 +0100

    can post and get artist by id

--- app.js
+++ app.js
@@ -2,9 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const bodyParser = require('body-parser');
-const artist = require('./controllers/Artist.js');
+const artistController = require('./controllers/Artist.js');
 
-console.log(artist);
 
 require('dotenv').config({
   path: path.join(__dirname, './settings.env'),
@@ -14,6 +13,8 @@ const app = express();
 mongoose.connect(process.env.DATABASE_CONN);
 app.use(bodyParser.json())
 app.get('/', (req, res) => res.send('Hello MongoDb!'));
-app.post('/Artist', artist.post);
+app.post('/Artist', artistController.post);
+app.get('/Artist', artistController.list);
+app.get('/Artist/:artistId', artistController.get);
 
 app.listen(3000, () => console.log('It works!'));
